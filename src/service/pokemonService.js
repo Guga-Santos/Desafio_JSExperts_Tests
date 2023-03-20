a753de8523b1244103c036fd88f4b1c855d074ff
GugaSantos <guga.santos.guga@gmail.com>
2023-03-20 01:44:37 -0300

    feat: create getDataLength function

--- src/service/pokemonService.js
+++ src/service/pokemonService.js
@@ -2,6 +2,11 @@ class PokemonService {
   constructor({ repository }) {
     this.repository = repository;
   }
+
+  async getDataLength() {
+    const data = await this.repository.getRawData();
+    return data.count;
+  }
 }
 
 module.exports = PokemonService;
